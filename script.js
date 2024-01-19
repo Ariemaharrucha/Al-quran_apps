46aa872413d2348df8751c3dfa8712542d762bc5
unknown <arie.maharucha.zakka@gmail.com>
2024-01-19 15:18:15 +0700

    add ayat

--- script.js
+++ script.js
@@ -5,18 +5,13 @@ const apiUrl = ``;
 
     fetchSurah();
 
-
     document.addEventListener('click', function (e) {
-        // console.log(e.target);
-        // if (e.target.dataset.test = 'test') {
-        //     console.log(e.target);
-        // }
-        
-        if(e.target.id === 'surah') {
-            // console.log(e.target.dataset.nomerSurah);
-            console.log(e.target.dataset.nomerSurah);
+        const dataSet_surah = e.target.closest('.surah')
+        if(dataSet_surah) {
+            console.log(dataSet_surah.dataset.nomer);
+             fetchAyatQuran(dataSet_surah.dataset.nomer);
+            
         }
-        
     });
 
 
@@ -41,7 +36,17 @@ const apiUrl = ``;
     }
 
     function fetchAyatQuran(nomer) {
+        fetch(`https://al-quran-8d642.firebaseio.com/surat/${nomer}.json?print=pretty`)
+        .then(response => response.json())
+        .then(result => {
+            let card = " "
+            result.forEach((a) => {
+                card += showAyat(a)
+            })
+            surahCotainer.innerHTML = card;
 
+        })
+        .catch(err => console.log(err))
     }
 
     function showSurah (surah) {
@@ -69,8 +74,8 @@ const apiUrl = ``;
       </div>`*/
 
       //v2
-      return `<div class="col-sm-4 mb-4" id="surah" data-nomerSurah="${surah.nomor}"> 
-      <div class="card btn p-3" >
+      return `<div class="col-sm-4 mb-4"> 
+      <div class="card surah btn p-3" type="button" data-nomer=${surah.nomor} >
             <div class="row justify-content-between " >                        
                 <div class=" col d-flex align-items-center">
                     <h4 class="nomer-surah ">${surah.nomor}</h4>
@@ -90,3 +95,36 @@ const apiUrl = ``;
     </div>
 </div> `
     }
+
+    // function updateUIayat(result) {
+    //     let card = " ";
+        
+    //     surahCotainer.innerHTML = card;  
+    //     console.log(result);      
+    // }
+
+    function showAyat(ayat) {
+        return `<div class="col-12 mb-4">
+        <div class="card " >
+          <div class="card-header row justify-content-between align-items-center">
+            <div class="col fs-4 fw-semibold">
+              ${ayat.nomor}
+            </div>
+            
+            <div class="col card-body text-bg-light p-2 text-end  "  >
+                <div class="card  p-2 text-end ">  
+                <p class="card-text  fs-2">${ayat.ar}</p>                             
+                </div>                
+            </div>
+
+          </div>
+          
+          <div class="card-body">
+            <p class="card-text font-monospace  fs-5 huruf-latin">${ayat.tr}</p>
+            <p class="card-text terjemahan fw-semibold">${ayat.id}.</p>
+            
+          </div>
+        </div>
+      </div>`
+    }
+
